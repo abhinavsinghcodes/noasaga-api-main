400e708e3c2f08f95dffc66c97d9cd75bc350dcd
Abhinav Singh <user67898a5@gmail.com>
2025-02-20 13:19:44 +0530

    FINAL

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use("/anime", routes);
+// Serve static files (CSS)
+app.use(express.static(path.join(__dirname, "src")));
 
+// Homepage Route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "src", "home.html"));
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
